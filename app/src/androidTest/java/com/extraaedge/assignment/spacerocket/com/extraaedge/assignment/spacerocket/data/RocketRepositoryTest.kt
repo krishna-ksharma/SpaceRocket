da132c2f9cd42c3c73d51e2ad9f55e1ed6cb8006
Krishna Sharma <krishna.ksharma84@gmail.com>
2023-03-24 11:20:58 +0530

    In Progress

--- app/src/androidTest/java/com/extraaedge/assignment/spacerocket/com/extraaedge/assignment/spacerocket/data/RocketRepositoryTest.kt
+++ app/src/androidTest/java/com/extraaedge/assignment/spacerocket/com/extraaedge/assignment/spacerocket/data/RocketRepositoryTest.kt
@@ -1,16 +1,22 @@
 package com.extraaedge.assignment.spacerocket.com.extraaedge.assignment.spacerocket.data
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.extraaedge.assignment.spacerocket.data.RocketRepository
+import com.extraaedge.assignment.spacerocket.data.RocketResult
 import com.extraaedge.assignment.spacerocket.data.local.RocketDatabase
 import com.extraaedge.assignment.spacerocket.data.remote.RocketApi
 import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.runBlocking
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import javax.inject.Inject
 import javax.inject.Named
 
+@HiltAndroidTest
 class RocketRepositoryTest {
 
     @get:Rule
@@ -39,6 +45,11 @@ class RocketRepositoryTest {
 
     @Test
     fun listRockets() {
+        val rocketRepository = RocketRepository(rocketApi, db.rocketDAo())
+        runBlocking {
+            val result = rocketRepository.listRockets(true)
+            Assert.assertEquals(true, result as RocketResult.Success)
 
+        }
     }
 }
