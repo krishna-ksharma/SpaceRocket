169746213700bc01136c2655c447feb0de997736
Krishna Sharma <krishna.ksharma84@gmail.com>
2023-03-24 22:49:05 +0530

    Repository test

--- app/src/androidTest/java/com/extraaedge/assignment/spacerocket/com/extraaedge/assignment/spacerocket/data/RocketRepositoryTest.kt
+++ app/src/androidTest/java/com/extraaedge/assignment/spacerocket/com/extraaedge/assignment/spacerocket/data/RocketRepositoryTest.kt
@@ -1,6 +1,8 @@
 package com.extraaedge.assignment.spacerocket.com.extraaedge.assignment.spacerocket.data
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.extraaedge.assignment.spacerocket.com.extraaedge.assignment.spacerocket.data.remote.MockServer
+import com.extraaedge.assignment.spacerocket.com.extraaedge.assignment.spacerocket.data.remote.enqueueResponse
 import com.extraaedge.assignment.spacerocket.data.RocketRepository
 import com.extraaedge.assignment.spacerocket.data.RocketResult
 import com.extraaedge.assignment.spacerocket.data.local.RocketDatabase
@@ -8,11 +10,7 @@ import com.extraaedge.assignment.spacerocket.data.remote.RocketApi
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.runBlocking
-import org.junit.After
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -41,15 +39,17 @@ class RocketRepositoryTest {
     @After
     fun tearDown() {
         db.close()
+        MockServer.server.shutdown()
     }
 
     @Test
     fun listRockets() {
         val rocketRepository = RocketRepository(rocketApi, db.rocketDAo())
+        MockServer.server.enqueueResponse("rockets_response.json", 200)
         runBlocking {
             val result = rocketRepository.listRockets(true)
-            Assert.assertEquals(true, result as RocketResult.Success)
-
+            Assert.assertEquals(true, result is RocketResult.Success)
+            Assert.assertEquals(1, (result as RocketResult.Success).data.size)
         }
     }
 }
